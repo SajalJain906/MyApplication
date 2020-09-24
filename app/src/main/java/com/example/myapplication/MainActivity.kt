927f63568eb5c5c6e9d139b028a250fa0cdda888
SajalJain906 <sajal.jain906@gmail.com>
2020-09-24 16:24:30 +0530

    making codes simpler

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val submission = findViewById<Button>(R.id.submit_button)
-        val editName = findViewById<EditText>(R.id.editTextTextPersonName)
+        //val submission = findViewById<Button>(R.id.submit_button)
+        //val editName = findViewById<EditText>(R.id.editTextTextPersonName)
 
-        submission.setOnClickListener{open_next_activity(name_id = editName) }
+        //submission.setOnClickListener{open_next_activity(name_id = editName) }
+        submit_button.setOnClickListener{open_next_activity(name_id = editTextTextPersonName) }
     }
 
     private fun open_next_activity(name_id: EditText) {

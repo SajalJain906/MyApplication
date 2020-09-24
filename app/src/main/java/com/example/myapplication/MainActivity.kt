97107629f120c82ac332ce3bb97ac0cc33487c23
SajalJain906 <sajal.jain906@gmail.com>
2020-09-24 18:37:52 +0530

    added camera permission

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,39 @@
 package com.example.myapplication
 
+import android.Manifest.permission
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        ActivityCompat.requestPermissions(this, arrayOf(CAMERA), 100)
+
         setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(this,permission.CAMERA) == PERMISSION_GRANTED){
+            Toast.makeText(this,"Application has permission for reading external storage",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            //val foo = ActivityCompat.shouldShowRequestPermissionRationale(this,permission.CAMERA)
+            Toast.makeText(this,"External storage permission is essential for this application",Toast.LENGTH_LONG).show()
+        }
 
         //val submission = findViewById<Button>(R.id.submit_button)
         //val editName = findViewById<EditText>(R.id.editTextTextPersonName)
